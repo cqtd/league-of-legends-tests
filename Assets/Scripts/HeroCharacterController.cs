db7afb5ec32b29043f8f20f776175373a326d5ce
cqtd <cqtd@protonmail.ch>
2020-08-12 13:37:28 +0900

    징크스 무브먼트 애니매이션

---
+++ Assets/Scripts/HeroCharacterController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class HeroCharacterController : MonoBehaviour
+{
+    public HeroAnimController animController;
+    public Rigidbody rb;
+
+    public Vector3 forwardVector;
+    public float speed = 10.0f;
+
+    bool isMoving;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            rb.velocity = forwardVector * speed * Time.deltaTime; 
+            // rb.AddForce();
+        }
+    }
+
+    public void Move()
+    {
+        animController.isMoving = true;
+        isMoving = true;
+    }
+
+    public void Idle()
+    {
+        animController.isMoving = false;
+        isMoving = false;
+    }
+    
+    
+}
