ddc3ab92c9d5cb782457bfc008e95afcf3aaba01
cqtd <cqtd@protonmail.ch>
2020-08-12 19:51:35 +0900

    카메라, 라이트맵, HDRI

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera[] cameras;
+    public int initialCamera = 0;
+
+    int current = 0;
+
+    void Awake()
+    {
+        SetCamera(initialCamera);
+    }
+
+    public void NextCamera()
+    {
+        int next = (current + 1) % cameras.Length;
+        current = next;
+        
+        SetCamera(next);
+    }
+
+    void SetCamera(int index)
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            cameras[i].gameObject.SetActive(index == i);
+        }
+    }
+}
