c48707fc24538823bf9db8d07f02a1f64ef8faf7
cqtd <cqtd@protonmail.ch>
2020-08-29 19:40:44 +0900

    0.0.1.25
    ## Changes
    - Hero shading model

--- Assets/BuildInfo.cs
+++ Assets/BuildInfo.cs
@@ -1,5 +1,5 @@
 public class BuildInfo {
 	public const string buildVersion = "0x272A";
-	public const string buildDate = "2020-08-28";
-	public const string buildTime = "08:40:59 PM";
+	public const string buildDate = "2020-08-29";
+	public const string buildTime = "07:30:36 PM";
 }
